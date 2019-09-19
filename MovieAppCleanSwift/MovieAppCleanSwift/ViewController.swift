90ae75041cde6f04e8c9e62de552e0eb1f157444
Kanchana Phakdeedorn <kanchana.pdk@gmail.com>
2019-09-19 18:14:23 +0700

    first commit

---
+++ MovieAppCleanSwift/MovieAppCleanSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieAppCleanSwift
+//
+//  Created by Kanchana Phakdeedorn on 19/9/2562 BE.
+//  Copyright © 2562 bobo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
